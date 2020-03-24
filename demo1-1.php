2b04e41a6a3b1b18b770db98a44d900051285d1d
filitov <filitov@gmail.com>
2020-03-25 02:13:27 +0800

    新增作業說明

---
+++ demo1-1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="zh_Hant">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>範例1-1</title>
+</head>
+<body>
+
+
+</body>
+</html>
---
+++ demo1-2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="zh_Hant">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>範例1-2</title>
+</head>
+<body>
+
+
+<img src="http://secret.tut.edu.tw/ezfiles/11/1011/img/911/logo.jpg" width="32" height="32">
+
+
+</body>
+</html>
